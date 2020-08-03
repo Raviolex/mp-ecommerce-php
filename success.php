63fad9880f03ecde1fadd69123978357eade4f9d
Raviolex <tomasgonzlp@gmail.com>
2020-08-03 17:28:10 -0300

    adada

--- success.php
+++ success.php
@@ -13,11 +13,12 @@ $access_token = 'APP_USR-2767046342004813-042418-0949c0cfe52c3dffb8b0ec2e3b59a68
 
 
 $payment_id = $_POST["payment_id"];
-
+var_dump($payment_id);
 $cURLConnection = curl_init();
 curl_setopt($cURLConnection, CURLOPT_URL, "https://api.mercadopago.com/v1/payments/$payment_id?access_token=$access_token");
 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($cURLConnection);
+echo $response;
 curl_close($cURLConnection);
 
 $jsonResponse = json_decode($response);
@@ -25,6 +26,8 @@ $jsonResponse = json_decode($response);
 $order_id = $jsonResponse->order->id;
 $payment_method_id = $jsonResponse->payment_method_id;
 $transaction_amount = $jsonResponse->transaction_amount;
+var_dump($payment_method_id);
+var_dump($transaction_amount);
 
 ?>
 
