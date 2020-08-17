c8e264302c32833a06c4a9a7c99313e40dc41688
Raviolex <tomasgonzlp@gmail.com>
2020-08-16 21:23:03 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -71,11 +71,8 @@ MercadoPago\SDK::setAccessToken("APP_USR-6300848884069710-050717-e6e82bb1dcba9f1
     $preference->external_reference = "ABCD1234";
 
     $preference->save();
-echo '<br><br><br><br><br>';
  
-
-  var_dump($preference);
-
+ 
 echo '<br><br><br><br><br>';
  
 
