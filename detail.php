9fd4bd0f6972f6f5a4adc845778555956d89b321
Raviolex <tomasgonzlp@gmail.com>
2020-08-18 20:38:11 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,10 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 //MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-MercadoPago\SDK::setAccessToken('TEST-6300848884069710-050717-cbb71b23e134fccd94ffef68216160a5-47398782');
-
-MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-
+MercadoPago\SDK::setAccessToken('APP_USR-6300848884069710-050717-e6e82bb1dcba9f15dc8d66459cf040ca-47398782');
+ 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
